cf1745ab34089a2c643853a3b22429a90a8249c3
Iain Dooley <iain@workingsoftware.com.au>
2018-02-28 12:46:12 +1100

    Removed calls to set active spreadsheet as this needs a container bound script then it will work

--- Common.js
+++ Common.js
@@ -13,16 +13,12 @@ function onOpen()
 ///////////////////////////////////////////////////////////////////////////////
 function doGet(e)
 {
-  var ss = SpreadsheetApp.openById("1t533HZLYKkXUNncJlRmJzf721CO65TikNdNsJDSbn5c")
-  SpreadsheetApp.setActiveSpreadsheet(ss);
   var htmlOut = doGetting(e);
   return htmlOut;
 }
 ///////////////////////////////////////////////////////////////////////////////
 function doPost(e)
 {
-  var ss = SpreadsheetApp.openById("1t533HZLYKkXUNncJlRmJzf721CO65TikNdNsJDSbn5c")
-  SpreadsheetApp.setActiveSpreadsheet(ss);
   var notifText = e.postData.contents;  
   var htmlOut = doPosting(notifText);
   return htmlOut;

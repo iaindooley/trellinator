f515baa0c397de00d72141911386dc97463a4c04
Iain Dooley <iain@workingsoftware.com.au>
2018-03-08 21:37:32 +1100

    Added new convenience function to get teh current week of the month

--- Trigger.js
+++ Trigger.js
@@ -4,3 +4,10 @@ Trigger.xMinutesFromNow = function(x)
 {
     return new Date(new Date().getTime() + x*60000);
 }
+
+// returns week of the month starting with 0
+Date.prototype.getWeekOfMonth = function() {
+  var firstWeekday = new Date(this.getFullYear(), this.getMonth(), 1).getDay();
+  var offsetDate = this.getDate() + firstWeekday - 1;
+  return Math.floor(offsetDate / 7);
+}

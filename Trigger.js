52258e5a283d516c6288be34dd0fbde29f4b1a19
Iain Dooley <iain@workingsoftware.com.au>
2018-03-12 09:48:32 +1100

    Added convenience function for getting a random number within some range

--- Trigger.js
+++ Trigger.js
@@ -33,3 +33,7 @@ Date.prototype.getMonthName = function()
     var monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
     return monthNames[this.getMonth()];
 }
+
+Trigger.getRandomArbitrary = function(min, max) {
+    return Math.random() * (max - min) + min;
+}

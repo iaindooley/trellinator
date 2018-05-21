e543cc0726f2ebb475b821d31d98eb0b80dc5694
Iain Dooley <iain@workingsoftware.com.au>
2018-05-22 09:13:04 +1000

    Fixed mysterious syntax error

--- TrigTest.js
+++ TrigTest.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const glob = require("glob")
+const glob = require("glob");
 const {exec} = require('child_process');
 var TrigTest = function(){};
 

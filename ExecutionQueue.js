d715b0a96510de41fb6211a06d0f9ee633384bb8
Iain Dooley <iain@workingsoftware.com.au>
2018-05-23 14:33:05 +1000

    fixed syntax error in new push function and added fake_push member so you can check the details that get pushed to the queue in your tests

--- ExecutionQueue.js
+++ ExecutionQueue.js
@@ -1,6 +1,10 @@
 var ExecutionQueue = function(){}
+ExecutionQueue.fake_push = null;
 
-ExecutionQueue.push(function_name,params,signature,dateobj)
+ExecutionQueue.push = function(function_name,params,signature,dateobj)
 {
-    push(dateobj,{functionName: function_name,parameters: params},signature);
+    if(!ExecutionQueue.fake_push)
+        push(dateobj,{functionName: function_name,parameters: params},signature);
+    else
+        ExecutionQueue.fake_push(function_name,params,signature,dateobj);
 }

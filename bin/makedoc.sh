e8deaf90fb7836da9b5b7df27e6675cbfe37a0ec
Iain Dooley <iain@workingsoftware.com.au>
2018-06-12 20:54:44 +1000

    Added in readme path

--- bin/makedoc.sh
+++ bin/makedoc.sh
@@ -1,3 +1,3 @@
-find trellinator* -name "*.js" | grep -v murphy | xargs jsdoc -c ~/jsdoc_conf.json
+find trellinator* -name "*.js" | grep -v murphy | xargs jsdoc -c ~/jsdoc_conf.json trellinator/README.md
 rm -rf /srv/www/www.trellinator.com/landing-promo/docs
 mv out/ /srv/www/www.trellinator.com/landing-promo/docs

ca9abc03b727390e14d5ecc175b48c7df6cb0dbe
Iain Dooley <iain@workingsoftware.com.au>
2018-06-19 18:06:06 +1000

    Put command to copy hello world tute into docs direcdtory

--- bin/makedoc.sh
+++ bin/makedoc.sh
@@ -1,3 +1,4 @@
 find trellinator* -name "*.js" | grep -v murphy | xargs jsdoc -c ~/jsdoc_conf.json trellinator/README.md
 rm -rf /srv/www/www.trellinator.com/landing-promo/docs
 mv out/ /srv/www/www.trellinator.com/landing-promo/docs
+cp ~/manual.docs.theprocedurepeople.com-Trellinator\ Hello\ World.pdf /srv/www/www.trellinator.com/landing-promo/docs/trellinator_hello_world.pdf

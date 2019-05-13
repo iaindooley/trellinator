f659244efcf714783d165c7cca527c8b4b63ea66
Iain Dooley <iain@workingsoftware.com.au>
2019-05-13 21:01:27 +1000

    Cope with empty postData, so that the buffer can easily forward a webhook that began life as a GET request

--- Common.js
+++ Common.js
@@ -43,24 +43,33 @@ function doGet(e)
 ///////////////////////////////////////////////////////////////////////////////
 function doPost(e)
 {
+
   var hooks = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Custom Webhooks");
   var htmlOut = false;
   
   if(hooks)
   {
     new IterableCollection(hooks.getDataRange().getValues()).each(function(row)
-                                     { 
-                                       if(this[row[0].trim()] &&(htmlOut === false))
-                                       {
-                                         htmlOut = this[row[0].trim()](e);
-                                       }
-                                     });
+                                                                  { 
+                                                                    if(this[row[0].trim()] &&(htmlOut === false))
+                                                                    {
+                                                                      htmlOut = this[row[0].trim()](e);
+                                                                    }
+                                                                  });
   }
   
   if(htmlOut === false)
   {
-    var notifText = e.postData.contents;  
-    var htmlOut = doPosting(notifText);
+    if(e.postData)
+    {
+      var notifText = e.postData.contents;  
+      var htmlOut = doPosting(notifText);
+    }
+    
+    else
+    {
+      var htmlOut = HtmlService.createHtmlOutput("<p>Processed Notification</p>");
+    }
   }
   
   else

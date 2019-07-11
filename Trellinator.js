005366fa21d1195e64315f694d3951095d7fbab9
Iain Dooley <iain@workingsoftware.com.au>
2019-07-12 09:13:45 +1000

    Added findOrCreateFolderByName and findOrCreateFileByName

--- Trellinator.js
+++ Trellinator.js
@@ -793,6 +793,138 @@ Trellinator.getFolderByURL = function(folderUrl)
   return folder;
 }
 
+Trellinator.findOrCreateFileByName = function(filename,creator,parent)
+{
+  if(!parent)
+  {
+    parent = DriveApp.getRootFolder();
+  }
+  
+  if(!creator)
+  {
+    creator = DocumentApp;
+  }
+  
+  try
+  {
+    var ret = Trellinator.getFileByName(filename,parent);
+  }
+  
+  catch(e)
+  {
+    Notification.expectException(InvalidDataException,e);
+    var ret = DriveApp.getFileById(creator.create(filename).getId());
+    DriveApp.getRootFolder().removeFile(ret);
+    parent.addFile(ret);
+  }
+  
+  return ret;
+}
+
+Trellinator.getFileByName = function(filename,parent)
+{
+  if(!parent)
+  {
+    parent = DriveApp.getRootFolder();
+  }
+
+  var iter = parent.getFilesByName(filename);
+  
+  if(iter.hasNext())
+  {
+    return iter.next();
+  }
+  
+  else
+  {
+    throw new InvalidDataException("No file: "+filename+" in parent: "+parent.getName());
+  }
+}
+
+Trellinator.findOrCreateFolderByName = function(filename,parent)
+{
+  if(!parent)
+  {
+    parent = DriveApp.getRootFolder();
+  }
+  
+  var id = Trellinator.getFolderByName(filename,parent);
+
+  if(!id.id)
+  {
+    var ret = parent.createFolder(filename);
+  }
+
+  else
+  {
+    var ret = DriveApp.getFolderById(id.id);
+  }
+
+  return ret;
+}
+
+Trellinator.getFolderByName = function(fileName, fileInFolder)
+{
+  var filecount = 0;
+  var dupFileArray = [];
+  var folderID = "";
+  
+  if(!fileInFolder)
+  {
+    fileInFolder = DriveApp.getRootFolder();
+  }
+
+  fileInFolder = fileInFolder.getId();
+  var files = DriveApp.getFoldersByName(fileName);
+
+  while(files.hasNext()){
+    var file = files.next();
+    dupFileArray.push(file.getId());
+
+    filecount++;
+  };
+
+  if(filecount > 1){
+    if(typeof fileInFolder === 'undefined'){
+        folderID = {"id":false,"error":"More than one file with name: "+fileName+". \nTry adding the file's folder name as a reference in Argument 2 of this function."}
+
+    }else{
+     //iterate through list of files with the same name
+     for(fl = 0; fl < dupFileArray.length; fl++){
+       var activeFile = DriveApp.getFileById(dupFileArray[fl]);
+       var folders = activeFile.getParents();
+       var folder = ""
+       var foldercount = 0;
+
+       //Get the folder name for each file
+       while(folders.hasNext()){
+         folder = folders.next().getName();
+         foldercount++;
+       };
+
+       if(folder === fileInFolder && foldercount > 1){
+         folderID = {"id":false,"error":"There is more than one parent folder: "+fileInFolder+" for file "+fileName}
+       };
+
+       if(folder === fileInFolder){
+           folderID = {"id":dupFileArray[fl],"error":false};
+
+       }else{
+         folderID = {"id":false,"error":"There are multiple files named: "+fileName+". \nBut none of them are in folder, "+fileInFolder}
+       };
+     };
+   };
+
+  }else if(filecount === 0){
+      folderID = {"id":false,"error":"No file in your drive exists with name: "+fileName};
+
+  }else{ //IF there is only 1 file with fileName
+    folderID = {"id":dupFileArray[0],"error":false};
+    };
+
+  return folderID;
+}
+
 /**
 * Static method to escape user input to be
 * used as part of a regular expression
